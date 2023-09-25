ce5f2c1a2db86d0ae59e9cbd7579776ef395c55b
Mohammad Tanzil Idrisi <idrisim@beloit.edu>
2023-09-25 15:57:17 -0500

    Button in Component

---
+++ components/Button.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Button extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Button</Text>
+      </View>
+    )
+  }
+}
+
+export default Button
